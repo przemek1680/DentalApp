8560e349e07dd65f24f8f7be538ae7e443b02299
PC_Przemek\IT <przemek1680@gmail.com>
2024-01-03 01:12:36 +0100

    Application init

---
+++ DentalApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DentalApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
